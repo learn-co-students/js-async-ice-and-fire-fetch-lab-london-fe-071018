9f51ea25475abce25b8baad64e6838523d4537b4
clairemacdonald <claire.macdonald@blis.com>
2018-09-06 11:21:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-function fetchBooks() {
-  //write fetch request to the Game of Thrones API
+function fetchBooks(){
+fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => {console.log(json);
+    renderBooks(json);
+  });
 }
 
+  //write fetch request to the Game of Thrones API
+
+
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
